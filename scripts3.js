f25e3c44864ac8f4f8491302089986e8fa7c16d7
Ichlaura <laupao41@gmail.com>
2021-02-13 01:49:17 -0500

    initial commit

--- scripts3.js
+++ scripts3.js
@@ -1,31 +1,24 @@
-function footwear() {
-    /* Input: today's weather
-    * Processing: find appropriate shoes for weather
-    * Output: Display appropriate shoes for weather to the user
-    */
-    
-    //Input: get user inputs and assign them to the variables
-    let hot = document.getElementById('hot').value;
-    let rain = document.getElementById('rain').value;
-    let snow = document.getElementById('snow').value;
-    let unknown = document.getElementById('unknown').value;
-    
-    //Processing: compare the value of the variable by using switch statsment
-    let shoes;
-    switch (weather) {
-    case hot:
-    shoes = 'sandals';
-    break;
-    case rain:
-    shoes = 'galoshes';
-    break;
-    case snow:
-    shoes = 'boots';
-    break;
-    case unknown:
-    shoes = 'shoes';
-    break;
-    }
-    //Output: Display the message to the user
-    document.getElementById('output').innerHTML = 'Adequate footwear type is ' + shoes + '.';
-}
+function footWear()
+		{
+			var answer = document.getElementById("weather").value;
+			var message;
+			
+			if (answer == 'hot' || answer =='Hot')
+			{
+				message = "sandals";
+			}
+			else if (answer == 'rain' || answer == 'Rain')
+			{
+				message = "galoshes";
+			}
+			else if ( answer == 'snow'|| answer == 'Snow')
+			{
+				message = "boots";
+			}		
+			else 
+			{
+				message = "shoes";
+			}
+
+			document.getElementById("output").innerHTML = message;
+		}
