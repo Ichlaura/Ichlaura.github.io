0b5039e01b47c8fb73bf8998f4d999e9bfa75a47
Ichlaura <laupao41@gmail.com>
2021-02-13 02:18:53 -0500

    initial commit

--- scripts3.js
+++ scripts3.js
@@ -1,40 +1,37 @@
 function footwear()
 		{
-            if (weather == "hot") {
-                footwear = "sandals";
-            } else if (weather == "rain") {
-                footwear = "galoshes";
-            } else if (weather == "snow") { 
-                footwear = "Boots";
-            } else {
-                footwear = "Shoes";
-            }
-    
-            let hot = document.getElementById('hot').value;
-            let rain = document.getElementById('rain').value;
-            let snow = document.getElementById('snow').value;
-            let unknown = document.getElementById('unknown').value;
 
-    
-            let shoes;
-            switch (weather) {
-            case hot:
-            shoes = 'sandals';
-            break;
-            case rain:
-            shoes = 'galoshes';
-            break;
-            case snow:
-            shoes = 'boots';
-            break;
-            case unknown:
-            shoes = 'shoes';
-            break;
 
-            document.getElementById('output').innerHTML = shoes; 
+//Input: get user inputs and assign them to the variables
+let hot = document.getElementById('hot').value;
+let rain = document.getElementById('rain').value;
+let snow = document.getElementById('snow').value;
+let unknown = document.getElementById('unknown').value;
+
+			let shoes;
+switch (weather) {
+case hot:
+shoes = 'sandals';
+break;
+case rain:
+shoes = 'galoshes';
+break;
+case snow:
+shoes = 'boots';
+break;
+case unknown:
+shoes = 'shoes';
+break;
+}
+//Output: Display the message to the user
+document.getElementById('output').innerHTML = 'Adequate footwear type is ' + shoes + '.';
+
+
+
+
         }
             
 
             
             
-        
+            
