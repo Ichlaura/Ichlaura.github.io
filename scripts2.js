193b9bfccc7bcd546aaae38ec8c9494d8a93b39a
Ichlaura <laupao41@gmail.com>
2021-02-12 23:04:06 -0500

    initial commit

---
+++ scripts2.js
@@ -0,0 +1,47 @@
+function check()
+		{
+			var first = parseFloat(document.getElementById('first').value);
+			var second = parseFloat(document.getElementById('second').value);
+			var op = document.getElementById('arithmetic').value;
+			var answer = parseFloat(document.getElementById('answer').value);
+
+			var compute;
+			var message;
+			if(op == "+"){
+				compute = first + second;
+				if ( answer == compute){
+					message = "Correct! Good job!";
+				} else {
+					message = "Incorrect. Try again!";
+				}
+			}
+			else if (op == "-"){
+				compute = first - second;
+				if ( answer == compute){
+					message = "Correct! Good job!";
+				} else {
+					message = "Incorrect. Try again!";
+				}
+			}
+			else if (op == "*"){
+				compute = first * second;
+				if ( answer == compute){
+					message = "Correct! Good job!";
+				} else {
+					message = "Incorrect. Try again!";
+				}
+			}
+			else if (op == "/"){
+				compute = first / second;
+				if ( answer == compute){
+					message = "Correct! Good job!";
+				} else {
+					message = "Incorrect. Try again!";
+				}
+			}
+			else {
+				message = "wrong operator";
+			}
+			document.getElementById("solution").innerHTML = message;
+		}
+		
