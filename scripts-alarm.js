28425c2696447b5322d305405a3a4f93ebd2e760
Ichlaura <laupao41@gmail.com>
2021-02-20 21:14:06 -0500

    initial commit

--- scripts-alarm.js
+++ scripts-alarm.js
@@ -1,10 +1,4 @@
 
-function alarm() {
-    var now = new Date();
-    var month = now.getMonth();
-    var dayOfMonth = now.getDate();
-    var dayOfWeek = now.getDay();
-    }
 
     function total()
 		{
