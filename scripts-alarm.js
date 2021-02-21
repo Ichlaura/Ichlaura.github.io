6e6743be04cc2a76619fce2e2aacdfe33b1f8b9e
Ichlaura <laupao41@gmail.com>
2021-02-20 20:56:04 -0500

    initial commit

--- scripts-alarm.js
+++ scripts-alarm.js
@@ -5,3 +5,29 @@ function alarm() {
     var dayOfMonth = now.getDate();
     var dayOfWeek = now.getDay();
     }
+
+    function total()
+		{
+			var dayOfWeek = new Date().getDay();
+			var subtotal = parseFloat(document.getElementById("subtotal").value);
+			var message;
+			var discout, tax;
+			
+			
+			if (subtotal >= 50 && (dayOfWeek == 2 || dayOfWeek == 3)){
+				
+				discout = subtotal * 0.1;
+				tax = discout * 0.06;
+				total = (subtotal - discout + tax).toFixed(2);
+				message = total;
+
+			}
+			else {
+			 
+			 	tax = subtotal * 0.06;
+			 	total = (subtotal + tax).toFixed(2);
+			 	message = total;
+
+			}
+			document.getElementById("solution").innerHTML = message;
+		}
