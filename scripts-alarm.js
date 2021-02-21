f214ab499828d1479f243c639c6c3ce7ffe93911
Ichlaura <laupao41@gmail.com>
2021-02-20 21:30:24 -0500

    initial commit

---
+++ scripts-alarm.js
@@ -0,0 +1,33 @@
+function dayFinder()
+{
+    var now = new Date();
+    var month = now.getMonth();
+    var dayOfMonth = now.getDate();
+    var dayOfWeek = now.getDay();
+    
+    var message;
+
+    if ( dayOfWeek != 0 || dayOfWeek != 6)
+    {
+        if( month == 0 && now == 1){
+            message = "Sleep in";
+        }
+
+        else if ( month == 6 && now == 4)
+        {
+            message = "Sleep in";
+        }
+
+        else if ( month == 11 && now == 25)
+        {
+            message == "Sleep in";
+        }
+
+        else 
+        {
+            message = "Get up!";
+        }
+    }
+        
+    document.getElementById("solution").innerHTML = message;
+}
