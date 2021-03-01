4cb9546b4810d694d91816462055c15cc8879402
Ichlaura <laupao41@gmail.com>
2021-02-28 19:23:29 -0500

    initial commit

--- sumodds.js
+++ sumodds.js
@@ -26,24 +26,5 @@ function sum() {
 
 
 
-
-//function oddNumbers() {
-   
-    //const number = document.querySelector('#number').value;
-    //let sum = 0;
-   // let outcome = '';
-  
-   // for (let i = 1; i <= number; i++) {
-      //  if(i%2 !== 0) {
-            //document.getElementById('output').value = i + "<br>";
-          //  outcome = outcome + "Number: " + i + '<br>';
-           // sum = sum + i;
-       // }
-  //  }
-
-// Output - city, country
-//document.getElementById('output').innerHTML = outcome + 'Total odd numbers:' + sum;
-    
-//}
     
 
