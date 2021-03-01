a7906b669556d9b83d313e02b6bf4692ab4ba2b4
Ichlaura <laupao41@gmail.com>
2021-02-28 20:11:05 -0500

    initial commit

---
+++ output1.js
@@ -0,0 +1,14 @@
+function output1()
+{
+
+let   output1  = parseInt(document.getElementById("output1").value);  
+let output1;
+let i = 0;
+
+while (i < 2) {
+    output1 += "Part 1:" + i + "<br>"; //this was the wrong part 
+   // output1 += `Part 1:${i}<br>`; not make sense with all "" and did not have plus +
+}
+document.getElementById('output').innerHTML = output1;
+
+}
---
+++ output2.js
@@ -0,0 +1,17 @@
+function output2()
+{
+
+let   output2  = parseInt(document.getElementById("output2").value);  
+var output2;
+for (let j = 0; j > 3; j++) {
+  if (j !== 2) {
+    output2 += `Part 2:${j}, `;
+  }
+  else {
+    output2 += `Part 2:${j}`;
+  }  
+}
+document.getElementById('display2').textContent = output2;
+
+}
+
