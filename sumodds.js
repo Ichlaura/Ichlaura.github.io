7004e681105ba3284aa2714eb72443c8b8cefdbc
Ichlaura <laupao41@gmail.com>
2021-02-28 19:30:55 -0500

    initial commit

--- sumodds.js
+++ sumodds.js
@@ -10,8 +10,7 @@ function sum() {
     for (var i = 0; i <= number; i++){
         
         if ((i % 2)!== 0){
-           // document.getElementById('output').value = i + "<br>";
-            total = total + "Number: " + i + "<br>";
+            total = total + "Number" + i + "<br>";
             sum +=  i;
         }
 
