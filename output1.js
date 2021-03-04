0157a7ef41dca1bcd10375cad5fd3faa254a843a
Ichlaura <laupao41@gmail.com>
2021-03-04 00:40:51 -0500

    initial commit

--- output1.js
+++ output1.js
@@ -2,13 +2,10 @@ function output1() {
 
     let output1 = " ";
     let i = 0;
-
     while (i < 2) {
         //  output1 += "Part 1:" + i + "<br>"; //this was the wrong part 
         output1 += `Part 1:${i}<br>`; //string 
         i++;
-        //not make sense with all "" and did not have plus +
     }
     document.getElementById('output1').innerHTML = output1;
-
 }
