cfdc60dc522162a01cb6f9adf25f9535b94f36d0
Ichlaura <laupao41@gmail.com>
2021-03-06 18:05:16 -0500

    initial commit

---
+++ fivetimes.js
@@ -0,0 +1,28 @@
+function fivetimes() {
+    let tables = document.getElementById("tables").value;
+
+
+    let table1 = 1;
+    let table2 = 2;
+    let table3 = 3;
+    let table4 = 4;
+    let table4 = 4;
+    let table5 = 5;
+    let table6 = 6;
+    let table7 = 7;
+    let table8 = 8;
+    let table9 = 9;
+    let table10 = 10;
+    let table11 = 11;
+    let table12 = 12;
+    let i= 0; 
+
+    if (tables1) {
+        message = table1*i;
+        i++;
+        }
+
+    document.getElementById('output').innerHTML = message;
+
+
+}
