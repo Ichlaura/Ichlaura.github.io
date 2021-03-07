d95bd6513d2b13cae4b61b7e75b121e76a1d51b4
Ichlaura <laupao41@gmail.com>
2021-03-06 21:52:50 -0500

    initial commit

--- question3.js
+++ question3.js
@@ -3,15 +3,14 @@ function table()
             let start = document.getElementById("start").value =true;
             let end = document.getElementById("end").value=true;
             let divisor = document.getElementById("divisor").value=true;
-			let message = "";
-       
-            for(start; end;)
-   while (start=0){
-       message += start /divisor;
-   }
+			let message= "";
+            let a = 0;
+            let b = 0;
+            
+while ( (a =0 )&& (b = 0))
+           a += start /divisor;
+           b += end /divisor;
 
-   
         
-        document.getElementById("output").innerHTML = message;
+        document.getElementById("output").innerHTML = a + b;
         }
-
