9e8314f786c01f52109f94d0fb791ebc154eb486
Ichlaura <laupao41@gmail.com>
2021-03-13 22:24:44 -0500

    initial commit

--- wind.js
+++ wind.js
@@ -1,15 +1,26 @@
 
+
+
+
+ //.........................Primera función .....................
  function doInputOutput() {
+     // Input: Pide la temperatura y el viento al usuario.
     var temperature = parseFloat(document.getElementById('temperature').value);
     var wind = parseFloat(document.getElementById('windspeed').value);
-    var result = f(temperature, wind);
+    // Input: las variables del resultado (result)  son iguales a temperatura y wind.
+    var result = f(temperature, wind); // la función es llamada.
+
+    //// Display the resulting for the user to see -  Muestra el resultado para que lo vea el usuario.
     document.getElementById('output').innerHTML = "Wind Chill: " + result.toFixed(2);
 }
 
+
+//..........................segunda función .......................
+// Determina el valor con la ecuación.
 function f(t, s) {
-  
+  // Ecuación 
  var f = 35.74 + (0.6215*t) - 35.75 * (Math.pow(s, 0.16)) + ((0.4275 * t) * (Math.pow(s, 0.16)));
 
- return f;
+ return f; // la función retorna al valor f de la ecuación.
 
 }
