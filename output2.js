f89add3671260d7a1595f70da70ffc7b973dfdfd
Ichlaura <laupao41@gmail.com>
2021-03-13 22:55:38 -0500

    initial commit

--- output2.js
+++ output2.js
@@ -1,8 +1,8 @@
 function output2() {
-  var output2;
+  let output2;
 for (let j = 0; j > 3; j++) {
   if (j !== 2) {
-    output2 += `Part 2:${j}, `;
+    output2 += `Part 2:${j}`;
   }
   else {
     output2 += `Part 2:${j}`;
