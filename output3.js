5067b19bac6d9b59a2afba0bafbeae6df0bfe119
Ichlaura <laupao41@gmail.com>
2021-03-13 22:52:39 -0500

    initial commit

--- output3.js
+++ output3.js
@@ -1,9 +1,8 @@
 function output3()
 { 
-let output3;
-let i=o;
-for ( i = 0; i < 4; i++) {
-  output3 += i + " ";
-}
-document.getElementById('output3').textContent = output3;
+  let output3;
+  for (let k = 0; k < 4; k++) {
+    output3 += k;
+  }
+  document.getElementById('output3').textContent = output3;
 }
