9397a6c09ae3b3a84eba45e2434b3beb6b4326c9
Ichlaura <laupao41@gmail.com>
2021-03-27 18:16:15 -0500

    initial commit

--- loan.js
+++ loan.js
@@ -1,47 +1,44 @@
 
-/* Input: Amount Borrowed, Annual Interest rate, Mumber of years, Payments per year, number of payments paid
- * Processing: 
- * Output: 
- */
+//Función doPayment
 function doPayment() {
-    //get input from user and define variables.
+    
     var principal = parseFloat(document.getElementById('principalInputBox').value);
     var annualRate = parseFloat(document.getElementById('annualRateInputBox').value);
     var years = parseFloat(document.getElementById('yearsInputBox').value);
     var periodsPerYear = parseFloat(document.getElementById('periodsPerYearInputBox').value);
-
-    // Determine the output and show it to the user.
     var result = computePayment(principal, annualRate, years, periodsPerYear);
+    result= result.toFixed(3);
     document.getElementById('outputPayment').innerHTML = result ;
 }
 
+//Función doBalance
 function doBalance() {
-    //get input from user and define variables.
+   
     var principal = parseFloat(document.getElementById('principalInputBox').value);
     var annualRate = parseFloat(document.getElementById('annualRateInputBox').value);
     var years = parseFloat(document.getElementById('yearsInputBox').value);
     var periodsPerYear = parseFloat(document.getElementById('periodsPerYearInputBox').value);
     var numberOfPaymentsPaidToDate = parseFloat(document.getElementById('numberOfPaymentsPaidToDateInputBox').value);
-
     var result = computeBalance(principal, annualRate, years, periodsPerYear, numberOfPaymentsPaidToDate);
+    result= result.toFixed(3);
     document.getElementById('outputBalance').innerHTML = result ;
 }
 
+//Función computePayment
 function computePayment(principal, annualRate, years, periodsPerYear) {
+
     var interestRatePerPeriod = annualRate / periodsPerYear;
     var periods = years * periodsPerYear;
     var numerator = principal * interestRatePerPeriod;
     var denomenatorParenthesis = 1 + interestRatePerPeriod;
     var denomenator = 1 - Math.pow(denomenatorParenthesis, -periods);
     var p = numerator / denomenator;
-//    var p = (principal * interestRatePerPeriod) / (1 - Math.pow(1 + interestRatePerPeriod, periods));
-    return p;
+    return "$" + p;
 }
 
+//Función computeBalance
 function computeBalance(principal, annualRate, years, periodsPerYear, numberOfPaymentsPaidToDate) {
-    var paymentPerPeriod = computePayment(principal, annualRate, years, periodsPerYear);
-    var interestRatePerPeriod = annualRate / periodsPerYear;
-    var b = principal * Math.pow(1 + interestRatePerPeriod, numberOfPaymentsPaidToDate) - ((Math.pow(1 + interestRatePerPeriod, numberOfPaymentsPaidToDate) - 1) / interestRatePerPeriod);
-    return b;
 
+    var b = principal * Math.pow((1 + (annualRate / periodsPerYear)), numberOfPaymentPaidToDate) - (((principal * (annualRate / periodsPerYear)) / (1 - Math.pow((1 + (annualRate / periodsPerYear)), - (years * periodsPerYear))) * (Math.pow(1 + (annualRate / periodsPerYear), numberOfPaymentPaidToDate) - 1)) / (annualRate / periodsPerYear))
+    return "$" + b;
 }
