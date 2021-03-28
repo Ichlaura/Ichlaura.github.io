4ea1f2da08e66a161dd1d1765b098870a2f1017a
Ichlaura <laupao41@gmail.com>
2021-03-27 22:09:17 -0500

    initial commit

--- output5.js
+++ output5.js
@@ -4,13 +4,15 @@ function output5()
     //document.write("En el siguiente ejemplo se puede observar la declaración while verdadera")
  //For this problem, just write a comment in your
  // submission of why this syntax is incorrect   
+ let n = " ";
 let i = 0;
-let n = 0;
 while (i < 3); {
-    i++;
-   n += n; // valid statements (this is not the problem)
+   
+   n += i; // valid statements (this is not the problem)
+   i++;
 }
-    document.getElementById('output5').textContent = n;
+document.getElementById('output5').innerHTML = n;
 
 }
+
 
