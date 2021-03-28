875fb61a30fc3438a74da361ddfff9c4bf2d532c
Ichlaura <laupao41@gmail.com>
2021-03-27 22:29:20 -0500

    initial commit

--- output5.js
+++ output5.js
@@ -1,18 +1,25 @@
 function output5()
 {
 
-    //document.write("En el siguiente ejemplo se puede observar la declaración while verdadera")
- //For this problem, just write a comment in your
- // submission of why this syntax is incorrect   
+//For this problem, just write a comment in your submission of why this syntax is incorrect 
+
+//ANSWER Example para que funcione 
+
  let n = " ";
 let i = 0;
-while (i < 3); {
+
+while (i < 3) { // había un ; acá, Here was the problem 
    
    n += i; // valid statements (this is not the problem)
    i++;
 }
-document.getElementById('output5').innerHTML = n;
 
+//Answer
+document.getElementById("output5").innerHTML = n;
+document.write("<br>En el siguiente ejemplo se puede observar la declaración while verdadera, este programa funciona porque el error se encontraba en la linea while (i<3);{<br><br>")
+document.write("let i = 0;<br>while (i < 3) {  // había un ; acá, Here was the problem <br>  // valid statements (this is not the problem)<br>i++;<br>}<br><br>")
+document.write("<br>The next loop we can look the statement while is true, this program works because the mistake was in line:  while (i<3);{<br><br>")
+document.write("let i = 0;<br>while (i < 3) {  // había un ; acá, Here was the problem because ; <br>  // valid statements (this is not the problem)<br>i++;<br>}")
 }
 
 
