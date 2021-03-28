d5d587d0858d29e1ec4397da8c47dd5e2ea2b983
Ichlaura <laupao41@gmail.com>
2021-03-27 21:16:27 -0500

    initial commit

--- wind.js
+++ wind.js
@@ -5,22 +5,28 @@
  //.........................Primera función .....................
  function doInputOutput() {
      // Input: Pide la temperatura y el viento al usuario.
-    var temperature = parseFloat(document.getElementById('temperature').value);
-    var wind = parseFloat(document.getElementById('windspeed').value);
+    var t = parseFloat(document.getElementById('temperature').value);
+    var s= parseFloat(document.getElementById('windspeed').value);
     // Input: las variables del resultado (result)  son iguales a temperatura y wind.
-    var result = f(temperature, wind); // la función f es llamada.
-
     //// Output: Display the resulting for the user to see -  Muestra el resultado para que lo vea el usuario.
-    document.getElementById('output').innerHTML = "Wind Chill: " + result.toFixed(3);
+    if (t < 50 && s >= 3) {
+      let result = windChill(t, s); // la función f es llamada.
+      document.getElementById('output').innerHTML = `${result.toFixed(0)}`;
+  }
 }
 
 
 //..........................Segunda función .......................
 // Determina el valor con la ecuación.
-function f(t, s) {
+function windChill(t, s) {
   // Ecuación 
- var f = 35.74 + (0.6215*t) - 35.75 * (Math.pow(s, 0.16)) + ((0.4275 * t) * (Math.pow(s, 0.16)));
-
+ //var f = 35.74 + (0.6215*t) - 35.75 * (Math.pow(s, 0.16)) + ((0.4275 * t) * (Math.pow(s, 0.16)));
+    var t = t;
+    var s = s;
+    var f= 35.74 + 0.6215 * t - 35.75 * Math.pow(s,0.16) + 0.4275 * t * Math.pow(s,0.16);
  return f; // la función retorna al valor f de la ecuación.
 
 }
+
+
+
